bff6ed48ab3df1128ad9a69d78e4e41968b5193e
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-07-07 07:26:31 +0700

    Initial commit

---
+++ lib/App/freqtable.pm
@@ -0,0 +1,12 @@
+package App::freqtable;
+
+# VERSION
+
+1;
+# ABSTRACT: Print frequency table of lines/words/characters/bytes/numbers
+
+=head1 SYNOPSIS
+
+See the command-line script L<freqtable>.
+
+=cut
